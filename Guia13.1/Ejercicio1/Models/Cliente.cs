67ca80c1a229836ad61e1c35c1bb8239914aadda
fernandofilipuzzi-dev <fernandofilipuzzi.dev@gmail.com>
2024-11-02 22:13:20 -0300

    .

---
+++ Guia13.1/Ejercicio1/Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio1.Models
+{
+    [Serializable]
+    public class Cliente:Ticket
+    {
+        private long dni;
+
+        static int numero;
+
+        public int TipoVehiculo { get; set; }
+
+        public Cliente(string dni)
+        {
+            nroOrden = ++numero;
+            this.dni = Convert.ToInt64( dni);//lanzar excepcion
+        }
+
+        public override string ToString() 
+        {
+            return $"CLIENTE;{base.ToString()};{dni}";
+        }
+    }
+}
---
+++ Guia13.1/Ejercicio1/Models/DniNoValidoException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    public class DniNoValidoException:ApplicationException
+    {
+        public DniNoValidoException() { }
+        public DniNoValidoException(string message):base(message) { }
+    }
+}
