a4731d432cdb2d977b9b6aec91fc2d91f07d6c88
fernandofilipuzzi-dev <fernandofilipuzzi.dev@gmail.com>
2024-11-04 19:59:54 -0300

    excepción

--- Guia13.1/Ejercicio1/Models/Cliente.cs
+++ Guia13.1/Ejercicio1/Models/Cliente.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Ejercicio1.Models
 {
@@ -14,7 +15,19 @@ namespace Ejercicio1.Models
         public Cliente(string dni)
         {
             nroOrden = ++numero;
+
+            bool hayNumeros = dni.Length != 0;
+            for (int n = 0; n < dni.Length && hayNumeros==true; n++)
+            {
+                hayNumeros &= char.IsNumber(dni[n]) == true;
+            }
+            if(hayNumeros==false) 
+                throw new DniNoValidoException("Hay caracteres no numéricos");
+
             this.dni = Convert.ToInt64( dni);//lanzar excepcion
+
+            if (this.dni < 5000000) 
+                throw new DniNoValidoException("El DNI debe ser mayor a 5000000");
         }
 
         public override string ToString() 
